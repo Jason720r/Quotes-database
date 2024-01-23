6ab1c573d1ff62d8ad5ce9978a6136e1ccba027d
Jason Li <jasonli99193@gmail.com>
2024-01-23 14:06:53 -0600

    progress

--- request_handler.py
+++ request_handler.py
@@ -9,7 +9,7 @@ class HandleRequests(BaseHTTPRequestHandler):
 
         print(self.path)
 
-        if self.path == "/workouts":
+        if self.path == "/quotes":
 
             response = [
                 {"id": 1, "quote": "Hello", "author": "Me"}, 
@@ -20,3 +20,10 @@ class HandleRequests(BaseHTTPRequestHandler):
 
         self.wfile.write(json.dumps(response).encode())
     def do_POST(self):
+
+        self._set_headers(201)
+
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        response = { "payload": post_body}
+        self.wfile.write(json.dumps(response).encode())
