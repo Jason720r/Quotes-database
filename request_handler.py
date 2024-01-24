edcd09f37c181de55c2e6ba0db3a89354aacd38d
Jason Li <jasonli99193@gmail.com>
2024-01-24 11:16:31 -0600

    configurations

--- request_handler.py
+++ request_handler.py
@@ -9,7 +9,7 @@ class HandleRequests(BaseHTTPRequestHandler):
 
         print(self.path)
 
-        if self.path == "/quotes":
+        if self.path == "/products":
 
             response = [
                 {"id": 1, "quote": "Hello", "author": "Me"}, 
@@ -37,3 +37,20 @@ class HandleRequests(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'application/json')
         self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
+    
+    def do_OPTIONS(self):
+
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+        self.send_header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept')
+        self.end_headers()
+
+def main():
+
+    host = ''
+    port = 8088
+    HTTPServer((host, port), HandleRequests).serve_forever()
+
+if __name__ == "__main__":
+    main()
