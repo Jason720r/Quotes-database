daf7ce61a5aeb81088d0c374894f430d2a5a8af4
Jason Li <jasonli99193@gmail.com>
2024-01-24 11:30:06 -0600

    get products

---
+++ views/product_requests.py
@@ -0,0 +1,14 @@
+PRODUCTS = [
+    {
+        "id": 1,
+        "typeId": 1,
+        "title": "rug",
+        "price": 25.99,
+        "deliveryTime": "2 weeks",
+        "inStock": 3
+
+    }
+]
+
+def get_all_products():
+    return PRODUCTS
