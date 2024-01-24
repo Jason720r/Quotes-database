961c4ef77fef5ec10e7fd49dd4d778925bcbf9d6
Jason Li <jasonli99193@gmail.com>
2024-01-24 11:52:13 -0600

    category request

---
+++ views/category_requests.py
@@ -0,0 +1,13 @@
+CATEGORIES = [
+    {
+        "id": 1,
+        "type": "Furniture"
+    },
+    {
+        "id": 2,
+        "type": "Kitchen"
+    }
+]
+
+def get_all_categories():
+    return CATEGORIES
