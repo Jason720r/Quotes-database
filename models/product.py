5ae4c439f4095b96d9054e5cebec9a92182a97a3
Jason Li <jasonli99193@gmail.com>
2024-01-25 12:33:43 -0600

    product model

---
+++ models/product.py
@@ -0,0 +1,9 @@
+class Product():
+
+    def __init__(self, id, typeId, title, price, deliveryTime, inStock):
+        self.id = id
+        self.typeId = typeId
+        self.title = title
+        self.price = price
+        self.deliveryTime = deliveryTime
+        self.inStock = inStock
