1a2b916b454fc0604f8c197665660a512d09b635
Jason Li <jasonli99193@gmail.com>
2024-01-29 10:17:37 -0600

    Order Model:

---
+++ models/order.py
@@ -0,0 +1,7 @@
+class Order():
+
+    def __init__(self, id, date, productId, userId):
+        self.id = id
+        self.date = date
+        self.productId = productId
+        self.userId = userId
